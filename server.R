574f968e9197eea7f025def953c2d9d7ac3cec34
thunder001 <zhengcl75@gmail.com>
2016-04-26 03:33:17 -0400

    update files

--- server.R
+++ server.R
@@ -7,12 +7,12 @@ set.seed(1345)
 intrain <- createDataPartition(Default$default, p=0.75, list=FALSE)
 training <- Default[intrain,]
 testing <- Default[-intrain,]
-dim(testing)
+#dim(testing)
 fit.glm <- glm(default~., data=training, family=binomial)
 glm.pred <- predict(fit.glm, testing)
 pred1 <- rep("No",2499)
 pred1[glm.pred>0.2] <- "Yes"
-table(pred1, testing$default)
+#table(pred1, testing$default)
 
 myfunction <- function(income, balance,stud_status) {
   dat <- data.frame(income=income, balance=balance, student=stud_status)
@@ -21,13 +21,13 @@ myfunction <- function(income, balance,stud_status) {
 }
 shinyServer(
   function(input, output) {
-    output$income <- renderPrint({if (input$goButton!=0) input$id1})
-    output$balance <- renderPrint({if (input$goButton!=0) input$id2})
-    output$stu_status <- renderPrint({if (input$goButton!=0) input$id3})
+    output$income <- renderPrint({input$goButton; isolate(input$id1)})
+    output$balance <- renderPrint({input$goButton; isolate(input$id2)})
+    output$stu_status <- renderPrint({input$goButton; isolate(input$id3)})
     output$prediction <- renderPrint({
       if (input$goButton==0) {
         "You haven't enter your information"
       }
-      else myfunction(input$id1,input$id2,input$id3)})
+      else isolate(myfunction(input$id1,input$id2,input$id3))})
     }
 )
--- ui.R
+++ ui.R
@@ -5,8 +5,13 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
     numericInput("id1", "Please enter your income", 0, min=0, max=200000, step=5000),
     numericInput("id2", "Please enter your balance", 0, min=0, max=100000, step=200),
-    selectInput("id3", "Are you a student?", c(" ","Yes", "No")),
-    actionButton("goButton", "Submit")
+    selectInput("id3", "Are you a student?", c("Yes", "No")),
+    actionButton("goButton", "Submit"),
+    p(" "),
+    p("This is a simple model to evaluate your risk for credit card default based on your balance, income and
+      if you are a student."),
+    p("Please enter or select your information, then press submit button, your input will immedietely show on the right.
+App will calculate your default probability.")
   ),
   mainPanel(
     h3("Result of prediction"),
